e2ba01f38833211995c6bf94fc1783c04d52742f
Mario-SO <gh_mario.proclaim@slmail.me>
2022-11-15 23:42:55 +0100

    0001 Two Sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod lc_0001_two_sum;
+pub use lc_0001_two_sum::*;
