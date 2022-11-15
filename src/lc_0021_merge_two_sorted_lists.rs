745a9cc45bcbf28e375dd841005b3f7a1b70f169
Mario-SO <gh_mario.proclaim@slmail.me>
2022-11-16 00:16:09 +0100

    0021 setup

---
+++ src/lc_0021_merge_two_sorted_lists.rs
@@ -0,0 +1,14 @@
+pub fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {}
+}
